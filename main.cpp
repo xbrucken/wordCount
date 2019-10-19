742380105429d20be2af14782ecf2ccf64093980
ThinMoon <565318723@qq.com>
2019-10-19 09:15:41 +0800

    定义好接口

---
+++ main.cpp
@@ -0,0 +1,107 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <map>
+#include <complex>
+#include<stack>
+#include <iomanip>
+using namespace std;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define rep(i,a,b) for(int i = (a); i < (b); ++i)
+#define per(i,b,a) for(int i = (b) - 1; i >= (a); --i)
+#define sz(a) (int)a.size()
+#define de(a) cout << #a << " = " << a << endl
+#define dd(a) cout << #a << " = " << a << " "
+#define all(a) a.begin(), a.end()
+#define pw(x) (1ll << (x))
+#define mst(a,b) memset(a,b,sizeof(a));
+#define lson rt<<1,l,mid
+#define rson rt<<1|1,mid+1,r
+typedef long long ll;
+typedef double db;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+const int inf=0x3f3f3f3f;
+void intput ()//huan
+{
+	
+}
+
+int count_characters(string )//lin
+{
+	
+}
+
+int count_line(string )//hu
+{
+	
+}
+
+int count_word(string )//wu
+{
+	
+}
+
+bool is_word(string )//zou
+{
+	
+}
+
+sting lowcase(string )//wang
+{
+	
+}
+
+int count_phrase(int m,string )//zhang
+{
+	
+}
+
+void output()//ye
+{
+	
+}
+
+int main()//wen
+{
+	
+} 
+
+
+bool cmp()//sort qiu
+{
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+ 	std::ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n,m;
+	cin>>n>>m;
+	 
+}
+
+
+
